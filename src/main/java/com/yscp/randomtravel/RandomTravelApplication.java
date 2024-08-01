78e8f8c16bc510bd4b74e81aedb8cbec423d2dbb
yushin <kkad45@naver.com>
2024-08-01 21:17:45 +0900

    init

---
+++ src/main/java/com/yscp/randomtravel/RandomTravelApplication.java
@@ -0,0 +1,13 @@
+package com.yscp.randomtravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomTravelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandomTravelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yscp/randomtravel/RandomTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yscp.randomtravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomTravelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
